f36dbdfd6fe6faf29c28e30c47437c57ba6d13fe
joaobenthin <joaopaulobenthin@hotmail.com>
2019-06-23 16:55:00 -0300

    fix mongodb

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 mongoose.connect(
-    'mongodb+srv://semana:semana@cluster0-miskq.mongodb.net/test?retryWrites=true&w=majority',
-    { useNewUrlParser: true }
+  "mongodb+srv://semana:semana@cluster0-miskq.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
 );
 
 app.use(cors());
@@ -19,12 +20,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use((req, res, next) => {
-    req.io = io;
-    
-    next();
+  req.io = io;
+
+  next();
 });
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
 server.listen(process.env.PORT);
- 
